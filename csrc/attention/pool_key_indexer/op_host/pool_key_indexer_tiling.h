7f69206310b77af85e75b717203c4e0344b23306
Li Jiahang <216526138+lijiahang226@users.noreply.github.com>
2026-08-28 16:59:33 +0800

    fix(csrc): drop unresolvable tiling_util include in pool_key_indexer
    The op_host/tiling_util.h include is not on the vLLM Ascend include path and the tiling code does not use it; remove it so pool_key_indexer_tiling.cpp compiles.
    
    Signed-off-by: Li Jiahang <216526138+lijiahang226@users.noreply.github.com>

--- csrc/attention/pool_key_indexer/op_host/pool_key_indexer_tiling.h
+++ csrc/attention/pool_key_indexer/op_host/pool_key_indexer_tiling.h
@@ -20,7 +20,6 @@
 #include "register/tilingdata_base.h"
 #include "tiling/platform/platform_ascendc.h"
 #include "tiling/tiling_api.h"
-#include "op_host/tiling_util.h"
 
 namespace optiling {
 enum class PkiDataLayout : uint32_t {
